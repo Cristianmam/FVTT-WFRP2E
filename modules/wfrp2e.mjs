88eccc16487a4d9bc5643e20aeb4a6f5722a96d1
Cristian <cristian.mambriani@yahoo.com.ar>
2026-01-23 16:07:17 -0300

    Added some basic comments

--- modules/wfrp2e.mjs
+++ modules/wfrp2e.mjs
@@ -2,16 +2,23 @@ import {WHCharacter} from "../documents/Character.mjs";
 
 import {WHCharacterSheet} from "../sheets/Character.mjs";
 
+//Check associated file for details on the schema
 import {CharacterDataModel} from "../data/Character.mjs";
 
 Hooks.once("init",function ()
 {
+    //You hook up the schema with their doctypes here, declare docks on system.json
     Object.assign(CONFIG.Actor.dataModels,
     {
+        //These badboys throw errors because of place holder fields tagged as required in the schema. The schema should be adapted to what we need
         character:CharacterDataModel
     });
-
-    console.log("Trying to register sheet");
+    //We can hookup custom classes to our documents like so
+    Object.assign(CONFIG.Actor.documentClass,
+    {
+        character:WHCharacter
+    });
+    //We can register sheets for our items/actors like so
     Actors.registerSheet('character',WHCharacterSheet,
     {
         types:["character"],
@@ -19,5 +26,3 @@ Hooks.once("init",function ()
         label:"Character Sheet"
     });
 });
-
-//CONFIG.Actor.character.documentClass = WHCharacter;
